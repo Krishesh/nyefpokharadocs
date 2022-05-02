a604aa5ddaf98d9670feda17d3086bea5c3dc628
Krishesh shrestha <shrestha.krishesh@gmail.com>
2022-05-02 22:18:12 +0545

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'NYEF POkhara'
+copyright = '2022, nyefpokhara'
+author = 'Krishesh shrestha'
 
 release = '0.1'
 version = '0.1.0'
